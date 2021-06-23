0e95ca3d536d4c5464baec78d2108e357b6a2372
Lívia Brasil <liviabrasil@id.uff.br>
2021-06-23 00:14:12 -0300

    database connected

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 import express from "express"
 import cors from "cors"
+import pg from "pg"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const { Pool } = pg;
+
+const connection = new Pool({
+    user: 'postgres',
+    password: '123456',
+    host: 'localhost',
+    port: 5432,
+    database: 'mywallet'
+})
+
 app.post('/login', (req,res) => {
-    // req.body { email, password } 
+    res.sendStatus(201)
 })
 
 app.post('/signup', (req,res) => {
-    // req.body { name, email, password }
+    const {name, email, password} = req.body
+    res.sendStatus(201)
 })
 
 app.get('/home', (req,res) => {
 })
 
-app.post('/entry', (req,res) => {
-    // req.body { value, description }
+app.post('/entry', (_req,res) => {
+    res.sendStatus(201)
 })
 
 app.post('/exit', (req,res) => {
